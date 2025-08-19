278bc164bc585d23a09af0fa72f9ed33f7d00762
Borges <abraao10clear@gmail.com>
2025-08-19 18:41:34 -0300

    Primeiro commit do projeto React

---
+++ src/pages/Movie/index.js
@@ -0,0 +1,9 @@
+function Movie() {
+  return (
+    <div>
+      <h1>Welcome to the Movie Page</h1>
+      <p>This page contains information about movies.</p>
+    </div>
+  );
+}
+export default Movie;
