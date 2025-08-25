cdd4ed0abf68dfe97bede833992acb93f46f7660
Borges <abraao10clear@gmail.com>
2025-08-25 19:05:43 -0300

    Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
+
+// URL DA API: movie/now_playing?api_key=1f044149653f684e988c7b4a718ca1f3&language=pt-BR
 
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("/movie/now_playing", {
+        params: {
+          api_key: "1f044149653f684e988c7b4a718ca1f3",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+
+      // console.log(response.data.results.slice(0, 10));
+
+      setFilmes(response.data.results.slice(0, 10));
+    }
+
+    loadFilmes();
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main entry point of our application.</p>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
